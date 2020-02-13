f5fa9fde99df21d4d9ff45978ca31685142504e3
Disha Sharma <dishasharma2801@gmail.com>
2020-02-13 12:41:46 -0500

    php project

---
+++ disha/project/header.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.bundle.js"></script>
+     
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+</head>
+<body>
+<div class="container-fluid bg-secondary-1">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 text-dark col-12 col-sm-12 justify-content-center">
+				<p class="m-0 py-2">welcome to DESTINATION OF SHOPPING.com</p>
+			</div>
+			<div class="col-md-9 col-12 col-sm-12   justify-content-md-end justify-content-center">
+				<div class="row">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<nav class="navbar nav-color navbar-expand-sm text-light sticky-top">
+	<a class="navbar-brand text-dark" href="#"><b><i>SHOPPING.com</i></b></a>
+	<button class="navbar-toggler bg-light" data-toggle="collapse" data-target="#menu">
+		<i class="fa fa-bars" aria-hidden="true"></i>
+	</button>
+
+
+	<div id="menu" class="collapse navbar-collapse">
+		<ul class="navbar-nav">
+			<li class="nav-item dropdown">
+				<a data-toggle="dropdown" class="px-md-4 mx-md-4 px-0 mx-0 nav-link font-weight-bold" href="#"><i class="fa fa-bars" aria-hidden="true"></i> Categories</a>
+				<div class="dropdown-menu">
+					<div class="dropdown-header">Main Categories</div>
+					<div class="dropdown-divider"></div>
+					<a href="#" class="dropdown-item">kids wear</a>
+					<a href="#" class="dropdown-item">womens wear</a>
+					<a href="#" class="dropdown-item">mens wear</a>
+				</div>
+
+
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="index.php">Home</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="about.php">About</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="contact.php">Contact</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Help</a>
+			</li>
+			
+		</ul>
+		
+		<ul class="navbar-nav ml-auto">
+			
+			<li class="nav-item">
+		         <a class="nav-link" href="login.php"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a>
+		    </li>
+            <li class="nav-item">
+                <a class="nav-link" href="signup.php"><i class="fa fa-user" aria-hidden="true"></i> Signup</a>
+            </li>
+        </ul>
+		    
+
+
+
+	</div>
+</nav>
+
+<div class="container-fluid">
+	<div class="container slider pt-4">
+		
+		<div class="row">
+			<div class="col-md-6 offset-md-3">
+				<div class="form-group">
+					<div class="input-group">
+						<input type="text" placeholder="Search" class="form-control" />
+						<div class="input-group-append">
+							<button class="btn btn-dark">
+								<i class="fa fa-search" aria-hidden="true"></i>
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+			
+
+	</div>
+</div>
+<div class="row px-2">
+			<img src="dddd.jpeg">
+			<p class="text-dark text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+		</div>
+		</div>
+	</div>
+</div>
+<div class="saperetor"></div>
