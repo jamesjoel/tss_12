32434a5787c1f5dc098540ef840609d2adf79c34
yogesh85s@gmail.com <yogesh85s@gmail.com>
2020-02-14 14:00:08 +0530

    new setup

---
+++ yogesh/project/aboutus.php
@@ -0,0 +1,22 @@
+<?php
+include("header.php")
+?>
+
+<div class="jumbotron">
+<h1 class="text-center">about us</h1>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-md-8 offset-md-2">
+<p>
+this is  a online shoping platform where you can buy or sale <br>
+product of your choice on great discounts.<p>
+</div>
+</div>
+</div>
+
+
+<?php
+
+include("footer.php")
+?>
---
+++ yogesh/project/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<?php
+include("header.php")
+?>
+<div class="jumbotron">
+	<h1 class="text-center">Recent Products</h1>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-3 col-6 col-sm-6">
+			<div class="card my-3">
+				<img src="images/1.png" class="card-img-top">
+
+				<div class="card-body">
+					<p class="font-weight-bold m-0 p-0">I-Phone</p>
+					<small>(<del>$ 199.00</del>)</small>
+					<small class="font-weight-bold">$ 159.00</small>
+				</div>
+				<div class="card-footer text-center">
+					<button class="btn btn-sm btn-blue">Buy</button>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-6 col-sm-6">
+			<div class="card my-3">
+				<img src="images/1.png" class="card-img-top">
+
+				<div class="card-body">
+					<p class="font-weight-bold m-0 p-0">I-Phone</p>
+					<small>(<del>$ 199.00</del>)</small>
+					<small class="font-weight-bold">$ 159.00</small>
+				</div>
+				<div class="card-footer text-center">
+					<button class="btn btn-sm btn-blue">Buy</button>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-6 col-sm-6">
+			<div class="card my-3">
+				<img src="images/1.png" class="card-img-top">
+
+				<div class="card-body">
+					<p class="font-weight-bold m-0 p-0">I-Phone</p>
+					<small>(<del>$ 199.00</del>)</small>
+					<small class="font-weight-bold">$ 159.00</small>
+				</div>
+				<div class="card-footer text-center">
+					<button class="btn btn-sm btn-blue">Buy</button>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-6 col-sm-6">
+			<div class="card my-3">
+				<img src="images/1.png" class="card-img-top">
+
+				<div class="card-body">
+					<p class="font-weight-bold m-0 p-0">I-Phone</p>
+					<small>(<del>$ 199.00</del>)</small>
+					<small class="font-weight-bold">$ 159.00</small>
+				</div>
+				<div class="card-footer text-center">
+					<button class="btn btn-sm btn-blue">Buy</button>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-6 col-sm-6">
+			<div class="card my-3">
+				<img src="images/1.png" class="card-img-top">
+
+				<div class="card-body">
+					<p class="font-weight-bold m-0 p-0">I-Phone</p>
+					<small>(<del>$ 199.00</del>)</small>
+					<small class="font-weight-bold">$ 159.00</small>
+				</div>
+				<div class="card-footer text-center">
+					<button class="btn btn-sm btn-blue">Buy</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php
+include("footer.php")
+?>
