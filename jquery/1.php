5c2420882ceeba1edb2b79162b91345de30880a0
jamesjoel <james.joelj@gmail.com>
2020-03-17 14:29:39 +0530

    jquery day 1

---
+++ jquery/1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript" src="jquery-3.4.1.min.js"></script>
+	<script>
+		$(document).ready(function(){
+
+			$("#a").click(function(){
+				alert("Indore");
+			})
+			$("#b").click(function(){
+				alert("ujjain");
+			})
+
+
+		});
+
+
+
+	</script>
+</head>
+<body>
+<button id="a">Hello</button>
+<h1>jQuery Day 1 </h1>
+<button id="b">OK</button>
+</body>
+</html>
